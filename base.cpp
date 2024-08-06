66c8afd19a47f4eb70ae43403f9ceaa0ef429342
Mentalistv <varngupta2001@gmail.com>
2024-08-06 18:48:59 +0530

    added file input

--- base.cpp
+++ base.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 #include <string>
+
 #include <functional>
 
+#include <fstream>
+#include <filesystem>
+
 using namespace std;
+namespace fs = std::filesystem;
 
 
 // datatypes
@@ -85,233 +91,252 @@ namespace std {
     };
 }
 
-void takeCertIp(Certificate* curr){
-    string id;
-    
-    string ct;
-
-    string pName;
-    int nameSize;
-    vector<string> lnName;
-
-    int iPSubject;
-    string pSubject = "";
-
-    string pNameSubject = "";
-    int lnSubjectSize;
-    vector<string> lnSubject;
-
-    int dBit = 0;
-
-
-    cin>>id;
-    curr->certID = id;
-
-    cin>>ct;
-    if(ct == "NAME")   curr->certType = NAME;
-    else    curr->certType = AUTH;
-
-    cin>>pName;
-    curr->name.issuer.key = pName;
-
-    cin>>nameSize;
-    lnName.resize(nameSize);
-    for(int i=0; i<nameSize; i++){
-        cin>>lnName[i];
-    }
-
-    cin>>iPSubject;
-    curr->subject.isPrincipal = iPSubject;
-
-    if(iPSubject){
-        cin>>pSubject;
-        curr->subject.principal.key = pSubject;
-    }
-    else{
-        cin>>pNameSubject;
-        curr->subject.name.issuer.key = pNameSubject;
-
-        cin>>lnSubjectSize;
-        for(int i=0; i<lnSubjectSize; i++){
-            cin>>lnSubject[i];
-        }
-    }
-
-    cin>>dBit;
-}
-
 
 // hash tables
-unordered_map<pair<Name, Subject>, unordered_set<Proof>> check;
-unordered_map<vector<string>, unordered_set<Proof>> value;
-unordered_map<vector<string>, unordered_set<Proof>> compatible;
+// unordered_map<pair<Name, Subject>, unordered_set<Proof*>> check;
+// unordered_map<vector<string>, unordered_set<Proof*>> value;
+// unordered_map<vector<string>, unordered_set<Proof*>> compatible;
 
 // hash table to store cert with id
 unordered_map<string, Certificate> certPool;
 
 // set to keep track of already loaded values
-unordered_set<vector<string>> loadedValue;
+// unordered_set<vector<string>> loadedValue;
 
 // additional table for unres
-// unordered_map<pair<Name, Subject>, unordered_set<Certificate>> check;
+// unordered_map<pair<Name, Subject>, unordered_set<Certificate>> ;
 
 
-// utility functions
+// input function
 
-void compatibleAddPrefix(Proof p){
-    vector<string> temp;
-    vector<string> name = p.subject.name.localNames;
-
-    for(int i=0; i<name.size(); i++){
-        temp.push_back(name[i]);
-
-        compatible[temp].insert(p);
-    }
-}
-
-vector<vector<string>> returnPrefix(vector<string> name){
-    vector<vector<string>> res;
-    vector<string> temp;
-
-    for(int i=0; i<name.size(); i++){
-        temp.push_back(name[i]);
-        res.push_back(temp);
+void takeCertIpFromFile(Certificate* curr, const string& filePath) {
+    ifstream file(filePath);
+    if (!file.is_open()) {
+        cerr << "Could not open the file: " << filePath << endl;
+        return;
     }
 
-    return res;    
-}
+    string id;
+    string ct;
+    string pName;
+    int nameSize;
+    vector<string> lnName;
+    int iPSubject;
+    string pSubject = "";
+    string pNameSubject = "";
+    int lnSubjectSize;
+    vector<string> lnSubject;
+    int dBit = 0;
 
-Proof* certToProof(Certificate c){
-    Proof* p = new Proof;
-a.push_back(43);
-    if(a == b)  cout<<"dsd";
-    p->certIDs.push_back(c.certID);
+    file >> id;
+    curr->certID = id;
 
-    p->delegationBit = c.delegationBit;
+    file >> ct;
+    if (ct == "NAME") {
+        curr->certType = NAME;
+    } else {
+        curr->certType = AUTH;
+    }
 
-    p->name = c.name;
+    file >> pName;
+    curr->name.issuer.key = pName;
 
-    p->subject = c.subject;
+    file >> nameSize;
+    lnName.resize(nameSize);
+    for (int i = 0; i < nameSize; i++) {
+        file >> lnName[i];
+    }
 
-    return p;
-}
+    file >> iPSubject;
+    curr->subject.isPrincipal = iPSubject;
 
+    if (iPSubject) {
+        file >> pSubject;
+        curr->subject.principal.key = pSubject;
+    } else {
+        file >> pNameSubject;
+        curr->subject.name.issuer.key = pNameSubject;
 
-// compose function
-
-Proof compose(Proof a, Proof b){
-    Proof* p = new Proof;
-
-    p->name.issuer.key = "composed";
-    p->name.localNames = a.name.localNames;
-
-    if(a.subject.name.localNames == b.name.localNames){
-        if(b.subject.isPrincipal){
-            p->subject.isPrincipal = true;
-            p->subject.principal = b.subject.principal;
-        }
-        else{
-            p->subject.isPrincipal = false;
-            p->subject.name = b.subject.name;
+        file >> lnSubjectSize;
+        lnSubject.resize(lnSubjectSize);
+        for (int i = 0; i < lnSubjectSize; i++) {
+            file >> lnSubject[i];
         }
     }
-    else{
-        p->subject.isPrincipal = false;
 
-        p->subject.name.localNames = b.subject.name.localNames;
+    file >> dBit;
 
-        int nameSize = b.name.localNames.size();
-        int subjectSize = a.subject.name.localNames.size();
+    file.close();
+}
+
+void processCertificatesFromFolder(const string& folderPath) {
+    for (const auto& entry : fs::directory_iterator(folderPath)) {
+        if (entry.is_regular_file()) {
+            Certificate cert;
+            takeCertIpFromFile(&cert, entry.path().string());
+            certPool[cert.certID] = cert;
+
+            // Process the certificate (for example, print it or store it in a list)
+            cout << "Processed certificate ID: " << cert.certID << endl;
+        }
+    }
+}
+
+
+// // utility functions
+
+// void compatibleAddPrefix(Proof p){
+//     vector<string> temp;
+//     vector<string> name = p.subject.name.localNames;
+
+//     for(int i=0; i<name.size(); i++){
+//         temp.push_back(name[i]);
+
+//         compatible[temp].insert(p);
+//     }
+// }
+
+// vector<vector<string>> returnPrefix(vector<string> name){
+//     vector<vector<string>> res;
+//     vector<string> temp;
+
+//     for(int i=0; i<name.size(); i++){
+//         temp.push_back(name[i]);
+//         res.push_back(temp);
+//     }
+
+//     return res;    
+// }
+
+// Proof* certToProof(Certificate c){
+//     Proof* p = new Proof;
+// a.push_back(43);
+//     if(a == b)  cout<<"dsd";
+//     p->certIDs.push_back(c.certID);
+
+//     p->delegationBit = c.delegationBit;
+
+//     p->name = c.name;
+
+//     p->subject = c.subject;
+
+//     return p;
+// }
+
+
+// // compose function
+
+// Proof compose(Proof a, Proof b){
+//     Proof* p = new Proof;
+
+//     p->name.issuer.key = "composed";
+//     p->name.localNames = a.name.localNames;
+
+//     if(a.subject.name.localNames == b.name.localNames){
+//         if(b.subject.isPrincipal){
+//             p->subject.isPrincipal = true;
+//             p->subject.principal = b.subject.principal;
+//         }
+//         else{
+//             p->subject.isPrincipal = false;
+//             p->subject.name = b.subject.name;
+//         }
+//     }
+//     else{
+//         p->subject.isPrincipal = false;
+
+//         p->subject.name.localNames = b.subject.name.localNames;
+
+//         int nameSize = b.name.localNames.size();
+//         int subjectSize = a.subject.name.localNames.size();
         
-        for(int i=nameSize; i<subjectSize; i++)
-            p->subject.name.localNames.push_back(a.subject.name.localNames[i]);
-    }
+//         for(int i=nameSize; i<subjectSize; i++)
+//             p->subject.name.localNames.push_back(a.subject.name.localNames[i]);
+//     }
 
-    p->delegationBit = a.delegationBit;
+//     p->delegationBit = a.delegationBit;
 
-    for(auto x: a.certIDs)
-        p->certIDs.push_back(x);
+//     for(auto x: a.certIDs)
+//         p->certIDs.push_back(x);
 
-    for(auto x: b.certIDs)
-        p->certIDs.push_back(x);
+//     for(auto x: b.certIDs)
+//         p->certIDs.push_back(x);
 
-    return *p;
-}
-
-
-// insert function
-
-vector<string> insert(Proof p){
-    if(!check.count({p.name, p.subject})){
-        // add in check
-        check[{p.name, p.subject}].insert(p);
-
-        // add in comaptible
-        if(!p.subject.isPrincipal){
-            compatibleAddPrefix(p);
-
-            vector<vector<string>> prefixName = returnPrefix(p.subject.name.localNames);
-
-            unordered_set<Proof> setProofValue;
-
-            for(auto x: prefixName){
-                for(auto y: value[x])
-                    setProofValue.insert(y);
-            }
-
-            for(auto x: setProofValue)
-                insert(compose(x, p));
-        }
+//     return *p;
+// }
 
 
-        else{
-            // for isPrincipal true
+// // insert function
 
-            // add in value
-            value[p.name.localNames].insert(p);
+// vector<string> insert(Proof p){
+//     if(!check.count({p.name, p.subject})){
+//         // add in check
+//         check[{p.name, p.subject}].insert(p);
 
-            unordered_set<Proof> setProofCompatible = compatible[p.name.localNames];
+//         // add in comaptible
+//         if(!p.subject.isPrincipal){
+//             compatibleAddPrefix(p);
 
-            for(auto x: setProofCompatible)
-                insert(compose(x, p));            
-        }
-    }
-}
+//             vector<vector<string>> prefixName = returnPrefix(p.subject.name.localNames);
 
+//             unordered_set<Proof> setProofValue;
 
-// load value function
+//             for(auto x: prefixName){
+//                 for(auto y: value[x])
+//                     setProofValue.insert(y);
+//             }
 
-void loadValue(vector<string> name){
-    // pick useful certs :: make proofs :: insert them :: return value[name] ::
-    if(!loadedValue.count(name)){
-        loadedValue.insert(name);
-
-        for(auto x: certPool){
-            if(x.second.name.localNames == name)
-        }
-
-    }
-}
+//             for(auto x: setProofValue)
+//                 insert(compose(x, p));
+//         }
 
 
-// name resolution
+//         else{
+//             // for isPrincipal true
 
-vector<string> nameResolution(vector<string> name){
+//             // add in value
+//             value[p.name.localNames].insert(p);
 
-}
+//             unordered_set<Proof> setProofCompatible = compatible[p.name.localNames];
+
+//             for(auto x: setProofCompatible)
+//                 insert(compose(x, p));            
+//         }
+//     }
+// }
+
+
+// // load value function
+
+// void loadValue(vector<string> name){
+//     // pick useful certs :: make proofs :: insert them :: return value[name] ::
+//     if(!loadedValue.count(name)){
+//         loadedValue.insert(name);
+
+//         for(auto x: certPool){
+//             if(x.second.name.localNames == name)
+//         }
+
+//     }
+// }
+
+
+// // name resolution
+
+// vector<string> nameResolution(vector<string> name){
+
+// }
 
 
 int main() {
-    int numberCert;
-    cin>>numberCert;
+    
+    string folderPath = "/home/varn/Downloads/MTP/code/certChnDscvry/certs";
+    processCertificatesFromFolder(folderPath);
 
-    for(int i=0; i<numberCert; i++){
-        Certificate *newCert = new Certificate;
-        takeCertIp(newCert);
-
-        if(certPool.count(newCert->certID))
-            certPool[newCert->certID] = *newCert;
+    for(auto x: certPool){
+        cout<<x.first<<" "<<x.second.name.issuer.key<<endl;
     }
+
 
 
 
